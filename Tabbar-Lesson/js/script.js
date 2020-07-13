59a38a43f69dba6233c4acded860bf967e30e6b1
nlllllly <unfmosp.y14@gmail.com>
2020-07-13 13:16:07 +0900

    [finish]1-11_Javascript

--- Tabbar-Lesson/js/script.js
+++ Tabbar-Lesson/js/script.js
@@ -1,10 +1,14 @@
-$(function() {
-    // メニュートリガーをクリックしたときにイベントを起こす
-    $('.menu-trigger').on('click', function(event) {
-        // メニュートリガーのactiveを追加するそのときには、id sp-manuはfadeする
-        // （元々sp-manu自体がdisplay:noneになっているため、ここではややこしいけど、fadeするとnavが表示されることになってる。）
-        $(this).toggleClass('active');
-        $('#sp-menu').fadeToggle();
-        event.preventDefault();
-    });
+$('#tab-contents .tab[id != "tab1"]').hide();
+
+// tab-menuのaタグがクリックされたときにイベントを実施
+$('#tab-menu a').on('click', function(event) {
+    // まず、表示されているcontetns自体をhideする
+    $("#tab-contents .tab").hide();
+    // 次に、選択されているtab-menuのactiveクラスを取って選択されていない状態にする。
+    $("#tab-menu .active").removeClass("active");
+    // クリックされたtab-menuをactiveクラスを追加して表示させる
+    $(this).addClass("active");
+    // クリックされたtab-menuのhrefで指定されているidの属性（tab1~4のどれか）を取得して、そのtabをshowする
+    $($(this).attr("href")).show();
+    event.preventDefault();
 });
